8e5d19b7fa0d9420d0c863d5e8def1880588d90c
eduardopaiva0 <paiva.eduardo@aluno.ifsp.edu.br>
2025-06-18 09:54:14 -0300

    Add files via upload

---
+++ Jogo do Acertou, Perdeu!.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int numOcult,in1,in2,numMin,numMax;
+	numMin = 1;
+	numMax = 100;
+	
+	do{
+		cout << "Digite o numero oculto entre 1 e 100: ";
+		cin >> numOcult;
+	}while(numOcult < 1 || numOcult > 100);
+	
+	while(in1 != numOcult && in2 != numOcult){
+		in2 = 0;
+		do{
+			cout << "Jogador 1 digite um numero entre " << numMin << " e " << numMax << " : ";
+			cin >> in1;
+		}while(in1 < numMin || in1 > numMax);
+		if(in1 > numOcult){
+			numMax = in1;
+		}else if(in1 < numOcult){
+			numMin = in1;
+		}else{
+			cout << "Jogador 1 perdeu!";
+			in2 = numOcult - 1;
+		}
+	
+		while(in2 < numMin || in2 > numMax ){
+			cout << "Jogador 2 digite um numero entre " << numMin << " e " << numMax << " : ";
+			cin >> in2;
+		}
+		if(in2 > numOcult){
+			numMax = in2;
+		}else if(in2 < numOcult){
+			numMin = in2;
+		}else{
+			cout << "Jogador 2 perdeu!";
+			in2 = numOcult;
+		}
+		
+	}
+}
